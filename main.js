5a5cb2d48840090db1c97fa84ab62fdb8db2f453
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-13 17:13:33 -0500

    Change players mark as the turn increases

--- main.js
+++ main.js
@@ -2,10 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
 var board = document.querySelector('.board');
 var squares = document.querySelectorAll('.square');
+var turn = 1;
+
+function checkTurn(){
+  if (turn % 2 === 0) {
+    return 'O';
+  } else {
+    return 'X';
+  }
+}
 
 board.addEventListener('click', function(e) {
-  e.target.innerText = 'x'
+
+  e.target.innerText = checkTurn();
+  turn ++;
 })
+
+
 
 
 })
