9e5cc58fadc813417dfc70e5bfade8d5a1864159
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-13 16:56:17 -0500

    Display mark on click

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
 var board = document.querySelector('.board');
 var squares = document.querySelectorAll('.square');
 
-
+board.addEventListener('click', function(e) {
+  e.target.innerText = 'x'
+})
 
 
 })
